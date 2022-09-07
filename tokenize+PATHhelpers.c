ba5e500d4ded46e5204e6188eb013686d424f379
YoungAuthorj <youngauthorjozef@yahoo.com>
2022-09-07 10:20:44 -0700

    Team

--- tokenize+PATHhelpers.c
+++ tokenize+PATHhelpers.c
@@ -42,12 +42,12 @@ int _splitPATH(char *str)
 
     for (i = 0; str[i]; i++)
     {
-        if (str[i] != : && searchflag == 1)
+        if (str[i] != ':' && searchflag == 1)
         {
             wordcount += 1;
             searchflag = 0;
         }
-        if (str[i + 1] == :)
+        if (str[i + 1] == ':')
         {
             searchflag = 1;
         }
@@ -65,7 +65,7 @@ int _PATHstrcmp(const char *s1, const char *s2)
 {
     int i;
 
-    for (i = 0; s2[i] != =; i++)
+    for (i = 0; s2[i] != '='; i++)
     {
         if (s1[i] != s2[i])
             return (-1);
@@ -89,5 +89,5 @@ char *_concat(char *tmp, char **av, char *tok)
     _strcat(tmp, tok);
     _strcat(tmp, "/");
     _strcat(tmp, av[0]);
-    tmp[len - 1] = 0;
+    tmp[len - 1] = '\0';
     return (tmp);
